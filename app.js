a7459fece754e311feb010ec9a1f3e0beb3a0af2
akki1947 <95550721+akki1947@users.noreply.github.com>
2026-03-07 14:15:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 
+async function loadNews(){
+
+const url="https://api.rss2json.com/v1/api.json?rss_url=https://economictimes.indiatimes.com/rssfeedsdefault.cms"
+
+const res=await fetch(url)
+
+const data=await res.json()
+
+const news=document.getElementById("news")
+
+news.innerHTML=""
+
+data.items.slice(0,10).forEach(item=>{
+
+const li=document.createElement("li")
+
+li.innerHTML=`<a href="${item.link}" target="_blank">${item.title}</a>`
+
+news.appendChild(li)
+
+})
+
+}
+
+loadNews()
