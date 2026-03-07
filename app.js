ed44afc3f71796ffaf15c9933e2280d9ae8b91b4
akki1947 <95550721+akki1947@users.noreply.github.com>
2026-03-07 14:35:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
+async function loadTicker(){
+
+try{
+
+const url="https://query1.finance.yahoo.com/v7/finance/quote?symbols=%5ENSEI,%5ENSEBANK"
+
+const res=await fetch(url)
+
+const data=await res.json()
+
+const results=data.quoteResponse.result
+
+const nifty=results.find(x=>x.symbol==="^NSEI")
+const bank=results.find(x=>x.symbol==="^NSEBANK")
+
+document.getElementById("nifty").innerText=
+nifty.regularMarketPrice+" ("+nifty.regularMarketChangePercent.toFixed(2)+"%)"
+
+document.getElementById("banknifty").innerText=
+bank.regularMarketPrice+" ("+bank.regularMarketChangePercent.toFixed(2)+"%)"
+
+}catch(e){
+
+console.log("Ticker error",e)
+
+}
+
+}
 
 async function loadNews(){
 
@@ -23,4 +51,7 @@ news.appendChild(li)
 
 }
 
+loadTicker()
 loadNews()
+
+setInterval(loadTicker,60000)
