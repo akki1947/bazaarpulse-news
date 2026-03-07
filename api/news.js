9f26388423a319f0c8f67808008cd97ca463c049
akki1947 <95550721+akki1947@users.noreply.github.com>
2026-03-07 15:02:19 +0530

    Update news.js

--- api/news.js
+++ api/news.js
@@ -1,1 +1,22 @@
 
+export default async function handler(req, res) {
+
+try {
+
+const url =
+"https://economictimes.indiatimes.com/rssfeedsdefault.cms"
+
+const r = await fetch(url)
+
+const xml = await r.text()
+
+res.setHeader("Access-Control-Allow-Origin", "*")
+res.status(200).send(xml)
+
+} catch (e) {
+
+res.status(500).json({ error: "Feed failed" })
+
+}
+
+}
